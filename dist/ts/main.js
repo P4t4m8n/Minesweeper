685565f57951a247a11e921d3bc97c0f16b4508a
P4t4m8n <eranopenuni@gmail.com>
2024-03-04 23:49:50 +0200

    dd svg for game modes

--- dist/ts/main.js
+++ dist/ts/main.js
@@ -54,9 +54,12 @@ function renderMarked(marked) {
     const elMarked = document.querySelector('.marked');
     elMarked.innerText = marked.toString();
 }
+function renderRestartSvg(svg) {
+    const elRestart = document.querySelector('.restart-svg');
+    elRestart.innerHTML = svg;
+}
 //EVENTS
 function onCellClick(ev, game) {
-    console.log("ev:", ev);
     ev.preventDefault();
     const target = ev.target;
     if (!target.classList.contains('cell'))
@@ -71,6 +74,7 @@ function onCellClick(ev, game) {
         game.startGame({ row, col });
         let lifes = game.getLifes();
         rednerLifes(lifes);
+        renderRestartSvg(_getWorriedSmiley());
     }
     const cell = game.getCellInstance(row, col);
     let renderType = `<span>0</span>`;
@@ -159,15 +163,28 @@ function onLevelChange(ev, game, size) {
     renderBoard(size);
 }
 function onGameOver(isWin, game) {
-    if (isWin)
+    if (isWin) {
         alert('Win');
+        renderRestartSvg(_getHappySMiley());
+    }
     else {
         alert('Lose');
         _revealMines(game.getBoard());
+        renderRestartSvg(_getSadSmiley());
     }
     game.gameOver(isWin);
 }
+function onRestart(ev, game) {
+}
 //Util
+function _revealMines(board) {
+    board.board.forEach((row, rowIdx) => row.forEach((cell, colIdx) => {
+        if (cell.getMine() && !cell.getShown()) {
+            cell.setShown();
+            renderCell(_getBombSvg(), rowIdx, colIdx);
+        }
+    }));
+}
 function _getMinesAmount(size) {
     let mines;
     switch (size) {
@@ -183,6 +200,7 @@ function _getMinesAmount(size) {
     }
     return mines;
 }
+//SVGS
 function _getBombSvg() {
     return (`<svg class="bomb" viewBox="0 0 512 512"><path d="M287.586 15.297l3.504 110.963 31.537-110.963h-35.04zm-95.78.238l-1.75 236.047-170.533-43.33L130.486 377.69l-88.77-5.174 114.432 112.357-44.466-75.867L186.896 417l-51.748-109.94 110.114 79.956-12.635-185.23.002.003 75.212 170.57 75.816-89.95-6.62 154.582 60.173-39.978-20.388 79.486 75.756-142.787-75.924 1.94L487.32 155.87l-131.402 73.08-12.264-139.69-65.41 140.336-86.435-214.06h-.003zM45.503 44.095L39.355 75.94 154.285 218h.002l-77.6-166.836-31.185-7.07zm422.27 24.776l-31.184 7.07-43.738 107.37 81.068-82.59-6.147-31.85zM279.208 403.61c-40.176 0-72.708 32.537-72.708 72.71 0 5.725.636 10.706 1.887 16.05 7.25-32.545 36.097-56.655 70.82-56.655 34.82 0 63.673 23.97 70.82 56.656 1.218-5.277 1.888-10.404 1.888-16.05 0-40.175-32.536-72.71-72.71-72.71z"/></svg>`);
 }
@@ -222,11 +240,32 @@ function _getNumberHtml(number) {
     }
     return `<span style="color:${color};" class="number">${number}</span>`;
 }
-function _revealMines(board) {
-    board.board.forEach((row, rowIdx) => row.forEach((cell, colIdx) => {
-        if (cell.getMine() && !cell.getShown()) {
-            cell.setShown();
-            renderCell(_getBombSvg(), rowIdx, colIdx);
-        }
-    }));
+function _getSmileySvg() {
+    return (`<svg class="restart-svg" viewBox="-2 -2 24 24" preserveAspectRatio="xMinYMin" class="jam jam-smiley">
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+    <path d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-3.92-4.606a1 1 0 1 1 1.84-.788 2.264 2.264 0 0 0 4.16 0 1 1 0 1 1 1.84.788 4.264 4.264 0 0 1-7.84 0zM7 6a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0V7a1 1 0 0 1 1-1zm6 0a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0V7a1 1 0 0 1 1-1z"></path></g>
+    </svg>`);
+}
+function _getWorriedSmiley() {
+    return (`<svg class="restart-svg" viewBox="0 0 256 256" >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> <path d="M76,108a16,16,0,1,1,16,16A16.00016,16.00016,0,0,1,76,108Zm88-16a16,16,0,1,0,16,16A16.00016,16.00016,0,0,0,164,92Zm72,36A108,108,0,1,1,128,20,108.12217,108.12217,0,0,1,236,128Zm-24,0a84,84,0,1,0-84,84A84.0953,84.0953,0,0,0,212,128Zm-44,20H88a12,12,0,0,0,0,24h80a12,12,0,0,0,0-24Z"></path> </g>
+        </svg>`);
+}
+function _getSadSmiley() {
+    return (`<svg  viewBox="0 0 256 256" id="Flat" >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> <path d="M128,20A108,108,0,1,0,236,128,108.12186,108.12186,0,0,0,128,20Zm0,192a84,84,0,1,1,84-84A84.09562,84.09562,0,0,1,128,212ZM76,108a16,16,0,1,1,16,16A16.00016,16.00016,0,0,1,76,108Zm104,0a16,16,0,1,1-16-16A16.00016,16.00016,0,0,1,180,108Zm-5.41113,54.18652a12,12,0,1,1-18.62793,15.13282,36.0032,36.0032,0,0,0-55.92237.001A12.0002,12.0002,0,1,1,81.41016,162.1875a60.0038,60.0038,0,0,1,93.17871-.001Z"></path> </g>
+        </svg>`);
+}
+function _getHappySMiley() {
+    return (`<svg viewBox="0 0 106.058 106.059" xml:space="preserve">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> <g> <path d="M90.547,15.518C69.859-5.172,36.199-5.172,15.515,15.513C-5.172,36.198-5.17,69.858,15.517,90.547 c20.682,20.684,54.342,20.684,75.028-0.004C111.23,69.858,111.228,36.2,90.547,15.518z M84.758,84.758 c-17.494,17.494-45.961,17.496-63.456,0.002c-17.498-17.497-17.496-45.966,0-63.46C38.797,3.807,67.262,3.805,84.76,21.302 C102.254,38.796,102.252,67.265,84.758,84.758z M33.24,38.671c0-3.424,2.777-6.201,6.201-6.201c3.422,0,6.199,2.776,6.199,6.201 c0,3.426-2.777,6.202-6.199,6.202C36.017,44.873,33.24,42.097,33.24,38.671z M61.357,38.671c0-3.424,2.779-6.201,6.203-6.201 s6.201,2.776,6.201,6.201c0,3.426-2.777,6.202-6.201,6.202S61.357,42.097,61.357,38.671z M76.017,62.068 c-2.512,5.805-7.23,10.254-13.006,12.652v3.94c0,5.295-4.471,9.587-9.982,9.587c-5.511,0-9.98-4.292-9.98-9.587v-3.932 c-5.863-2.405-10.594-6.885-13.023-12.734c-0.637-1.529,0.09-3.285,1.619-3.921c0.377-0.155,0.766-0.229,1.15-0.229 c1.176,0,2.291,0.695,2.771,1.85c2.777,6.686,9.654,11.004,17.523,11.004c7.689,0,14.527-4.321,17.42-11.011 c0.658-1.521,2.424-2.222,3.944-1.563C75.974,58.781,76.675,60.548,76.017,62.068z"></path> </g> </g>
+        </svg>`);
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -83,9 +83,13 @@ function renderMarked(marked: number): void {
     elMarked.innerText = marked.toString()
 }
 
+function renderRestartSvg(svg: string): void {
+    const elRestart = document.querySelector('.restart-svg') as HTMLOrSVGScriptElement
+    elRestart.innerHTML = svg
+}
+
 //EVENTS
 function onCellClick(ev: Event, game: Game): void {
-    console.log("ev:", ev)
 
     ev.preventDefault()
 
@@ -106,6 +110,7 @@ function onCellClick(ev: Event, game: Game): void {
         game.startGame({ row, col })
         let lifes = game.getLifes()
         rednerLifes(lifes)
+        renderRestartSvg(_getWorriedSmiley())
     }
 
     const cell = game.getCellInstance(row, col)
@@ -221,15 +226,34 @@ function onLevelChange(ev: Event, game: Game, size: number) {
 }
 
 function onGameOver(isWin: boolean, game: Game) {
-    if (isWin) alert('Win')
+    if (isWin) {
+        alert('Win')
+        renderRestartSvg(_getHappySMiley())
+    }
     else {
         alert('Lose')
         _revealMines(game.getBoard())
+        renderRestartSvg(_getSadSmiley())
     }
     game.gameOver(isWin)
 }
 
+function onRestart(ev:Event,game:Game):void{
+
+}
+
 //Util
+
+function _revealMines(board: Board): void {
+    board.board.forEach((row, rowIdx) =>
+        row.forEach((cell, colIdx) => {
+            if (cell.getMine() && !cell.getShown()) {
+                cell.setShown()
+                renderCell(_getBombSvg(), rowIdx, colIdx)
+            }
+        }))
+
+}
 
 function _getMinesAmount(size: number): number {
     let mines: number
@@ -251,6 +275,8 @@ function _getMinesAmount(size: number): number {
 
     return mines
 }
+
+//SVGS
 
 function _getBombSvg(): string {
     return (
@@ -303,15 +329,47 @@ function _getNumberHtml(number: number): string {
 
 }
 
-function _revealMines(board: Board): void {
-    board.board.forEach((row, rowIdx) =>
-        row.forEach((cell, colIdx) => {
-            if (cell.getMine() && !cell.getShown()) {
-                cell.setShown()
-                renderCell(_getBombSvg(), rowIdx, colIdx)
-            }
-        }))
-
+function _getSmileySvg(): string {
+    return (
+        `<svg class="restart-svg" viewBox="-2 -2 24 24" preserveAspectRatio="xMinYMin" class="jam jam-smiley">
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+    <path d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-3.92-4.606a1 1 0 1 1 1.84-.788 2.264 2.264 0 0 0 4.16 0 1 1 0 1 1 1.84.788 4.264 4.264 0 0 1-7.84 0zM7 6a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0V7a1 1 0 0 1 1-1zm6 0a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0V7a1 1 0 0 1 1-1z"></path></g>
+    </svg>`
+    )
 }
+
+function _getWorriedSmiley(): string {
+    return (
+        `<svg class="restart-svg" viewBox="0 0 256 256" >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> <path d="M76,108a16,16,0,1,1,16,16A16.00016,16.00016,0,0,1,76,108Zm88-16a16,16,0,1,0,16,16A16.00016,16.00016,0,0,0,164,92Zm72,36A108,108,0,1,1,128,20,108.12217,108.12217,0,0,1,236,128Zm-24,0a84,84,0,1,0-84,84A84.0953,84.0953,0,0,0,212,128Zm-44,20H88a12,12,0,0,0,0,24h80a12,12,0,0,0,0-24Z"></path> </g>
+        </svg>`
+    )
+}
+
+function _getSadSmiley(): string {
+    return (
+        `<svg  viewBox="0 0 256 256" id="Flat" >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> <path d="M128,20A108,108,0,1,0,236,128,108.12186,108.12186,0,0,0,128,20Zm0,192a84,84,0,1,1,84-84A84.09562,84.09562,0,0,1,128,212ZM76,108a16,16,0,1,1,16,16A16.00016,16.00016,0,0,1,76,108Zm104,0a16,16,0,1,1-16-16A16.00016,16.00016,0,0,1,180,108Zm-5.41113,54.18652a12,12,0,1,1-18.62793,15.13282,36.0032,36.0032,0,0,0-55.92237.001A12.0002,12.0002,0,1,1,81.41016,162.1875a60.0038,60.0038,0,0,1,93.17871-.001Z"></path> </g>
+        </svg>`
+    )
+}
+
+function _getHappySMiley(): string {
+    return (
+        `<svg viewBox="0 0 106.058 106.059" xml:space="preserve">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> <g> <path d="M90.547,15.518C69.859-5.172,36.199-5.172,15.515,15.513C-5.172,36.198-5.17,69.858,15.517,90.547 c20.682,20.684,54.342,20.684,75.028-0.004C111.23,69.858,111.228,36.2,90.547,15.518z M84.758,84.758 c-17.494,17.494-45.961,17.496-63.456,0.002c-17.498-17.497-17.496-45.966,0-63.46C38.797,3.807,67.262,3.805,84.76,21.302 C102.254,38.796,102.252,67.265,84.758,84.758z M33.24,38.671c0-3.424,2.777-6.201,6.201-6.201c3.422,0,6.199,2.776,6.199,6.201 c0,3.426-2.777,6.202-6.199,6.202C36.017,44.873,33.24,42.097,33.24,38.671z M61.357,38.671c0-3.424,2.779-6.201,6.203-6.201 s6.201,2.776,6.201,6.201c0,3.426-2.777,6.202-6.201,6.202S61.357,42.097,61.357,38.671z M76.017,62.068 c-2.512,5.805-7.23,10.254-13.006,12.652v3.94c0,5.295-4.471,9.587-9.982,9.587c-5.511,0-9.98-4.292-9.98-9.587v-3.932 c-5.863-2.405-10.594-6.885-13.023-12.734c-0.637-1.529,0.09-3.285,1.619-3.921c0.377-0.155,0.766-0.229,1.15-0.229 c1.176,0,2.291,0.695,2.771,1.85c2.777,6.686,9.654,11.004,17.523,11.004c7.689,0,14.527-4.321,17.42-11.011 c0.658-1.521,2.424-2.222,3.944-1.563C75.974,58.781,76.675,60.548,76.017,62.068z"></path> </g> </g>
+        </svg>`
+    )
+}
+
+
 
 
